7638e341b451b7bee81acaba645c76ac86fc331a
Emily Ellison <emilyellison986@gmail.com>
2012-05-30 21:19:46 -0500

    Add Sign-Up to C&V and Add a Header Menu

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,20 @@
+class MembersController < ApplicationController
+
+  def new
+    @member = Member.new
+  end
+  
+  def create
+    @member = Member.new(params[:member])
+    if @member.save
+      redirect_to root_url, notice: "Congrats on your CatBribes membership, #{@member.first}!"
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @member = Member.find_by_id(params[:id])
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 CatBribesApp::Application.routes.draw do
 
   root to: 'catbribes#index'
+  
   resources :catbribes, only: [ :index ]
+  resources :members, only: [ :new, :create, :show ]
 
 end
