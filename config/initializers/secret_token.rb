6eb2b3984fd84f3ead10cb236d075ec82dda7f20
Emily Ellison <emilyellison986@gmail.com>
2012-05-30 20:51:27 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatBribesApp::Application.config.secret_token = 'cf75522b0e92e7d708ec6c206e2b1d2af72332384640f0d19d3789954a1becfd8b93a502bfcb08817cd11d19e504534fa89e0b8721f044769bc8234a01588194'
