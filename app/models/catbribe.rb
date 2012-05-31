1f5b442cf83161907a189d94655d57d7a4b99710
Emily Ellison <emilyellison986@gmail.com>
2012-05-30 22:30:33 -0500

    Add CatBribe Model Validations

--- app/models/catbribe.rb
+++ app/models/catbribe.rb
@@ -1,7 +1,10 @@
 class Catbribe < ActiveRecord::Base
   attr_accessible :caption, :image, :member_id
   belongs_to :member
-  
   mount_uploader :image, ImageUploader
+  
+  validates :image, presence: true
+  validates :caption, presence: true
+  validates :member_id, presence: true
   
 end
