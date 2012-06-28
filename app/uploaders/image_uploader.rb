6e1e6d54d8ae441ae4a12a266c1f8d06e4d40778
Emily Ellison <emilyellison986@gmail.com>
2012-06-28 15:36:26 -0500

    Set Serve Static Assets to True

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,7 +16,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
+  def cache_dir
     "#{Rails.root}/tmp/uploads"
   end
 
